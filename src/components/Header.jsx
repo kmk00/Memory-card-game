eb2887cb26526bc422b940d8fb76ba13b7885718
kmk00 <73724318+kmk00@users.noreply.github.com>
2023-01-02 19:11:49 +0100

    Create header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="p-4 flex justify-center">
+      <img src="../../../public/logo.png" className="h-12"></img>
+    </header>
+  );
+}
+
+export default Header;
