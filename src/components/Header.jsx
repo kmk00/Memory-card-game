3cb439fe0cd80dcdfdd0062db3ad534076d1385c
kmk00 <73724318+kmk00@users.noreply.github.com>
2023-01-04 17:02:24 +0100

    Fix logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import logo from "../assets/logo.png";
 
 function Header() {
   return (
     <header className="p-6 flex justify-center md:justify-start bg-zinc-900 w-full">
-      <img src="../assets/logo.png" className="h-12"></img>
+      <img src={logo} className="h-12"></img>
     </header>
   );
 }
